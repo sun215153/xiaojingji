f9b762123260b23a8b63e125188e71bf6a5fcb2c
joke <sun215153@sina.com>
2013-12-21 13:36:49 +0800

    Signed-off-by: joke <sun215153@sina.com>

--- document/crebas.sql
+++ document/crebas.sql
@@ -1,6 +1,6 @@
 /*==============================================================*/
 /* DBMS name:      MySQL 5.0                                    */
-/* Created on:     2013/12/19 13:44:53                          */
+/* Created on:     2013/12/21 13:35:07                          */
 /*==============================================================*/
 
 
@@ -11,7 +11,7 @@ drop table if exists user;
 /*==============================================================*/
 create table user
 (
-   userid               bigint not null,
+   userid               bigint not null auto_increment,
    username             varchar(30),
    password             varchar(30),
    email                varchar(200),
